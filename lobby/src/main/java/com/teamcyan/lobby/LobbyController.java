2f904c681a53808c9f9bb4748422c66ec62936db
Frank Ikenna Okeke <okeke@rhrk.uni-kl.de>
2023-11-26 14:47:00 +0100

    made the corrected references to controller and fxml. implementations coming soon....

--- lobby/src/main/java/com/teamcyan/lobby/LobbyController.java
+++ lobby/src/main/java/com/teamcyan/lobby/LobbyController.java
@@ -1,4 +1,35 @@
 package com.teamcyan.lobby;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class LobbyController {
+
+    @FXML
+    private AnchorPane card;
+
+    @FXML
+    private ImageView backgroundView;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button sign_in;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button guest;
+
+
+
 }
