237180eb1ad2fa7b3844325a581a26963404fd6b
Frank Ikenna Okeke <okeke@rhrk.uni-kl.de>
2023-11-26 15:40:05 +0100

    exit method implemented. Frank and Jerry

--- lobby/src/main/java/com/teamcyan/lobby/LobbyController.java
+++ lobby/src/main/java/com/teamcyan/lobby/LobbyController.java
@@ -1,9 +1,8 @@
 package com.teamcyan.lobby;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -38,6 +37,20 @@ public class LobbyController {
 
     @FXML
     void exitGame(MouseEvent event) {
+        // Display a confirmation dialog before exiting the game
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit Game");
+        alert.setHeaderText("Are you sure you want to exit the game?");
+        alert.setContentText("Any unsaved progress may be lost.");
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                // User clicked OK, exit the game gracefully
+                Platform.exit();
+            } else {
+                // User clicked Cancel or closed the dialog, do nothing
+            }
+        });
 
     }
 
