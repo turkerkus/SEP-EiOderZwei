3caf9fddf08e8a1bb604b34f7a86ac5019d68213
Denis Ahmeti <d_ahmeti20@informatik.uni-kl.de>
2023-11-28 15:07:20 +0000

    Revert "Changes from Türker: Spieltisch GUI added"
    This reverts commit 835aced5b15a368fb0dfc88d1b10e65b4f66b044

--- SpieltischGUI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.spieltischgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.spieltischgui to javafx.fxml;
-    exports com.example.spieltischgui;
-}
