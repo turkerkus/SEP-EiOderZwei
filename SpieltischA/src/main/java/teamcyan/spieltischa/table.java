bee32ffabe274c0621b40667cb91f9f33b2b7924
Türker <t_erkus20@cs.uni-kl.de>
2023-11-28 23:27:56 +0100

    INCREMENT

--- SpieltischA/src/main/java/teamcyan/spieltischa/table.java
+++ SpieltischA/src/main/java/teamcyan/spieltischa/table.java
@@ -7,21 +7,22 @@ import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-
+// Programm, um den Spieltisch für das Spiel anzuzeigen.
 public class table extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // Die Bilder werden aus dem Ressources Ordner importiert und ihre Größe wird verändert.
+        // Durch StackPane sind die Bilder zentriert postioniert.
         StackPane stackPane = new StackPane();
         Image image = new Image(getClass().getResourceAsStream("/images/bg.png"));
         Image image2 = new Image(getClass().getResourceAsStream("/images/table.png"));
         ImageView imageView2 = new ImageView(image2);
         ImageView imageView = new ImageView(image);
-
         imageView2.setFitHeight(1200);
         imageView2.setFitWidth(1200);
         imageView2.setPreserveRatio(true);
-
+        //Das Hintergrundbild wird an die Fenstergröße angepasst.
         imageView.fitWidthProperty().bind(primaryStage.widthProperty());
         imageView.fitHeightProperty().bind(primaryStage.heightProperty());
 
@@ -30,7 +31,7 @@ public class table extends Application {
         stackPane.getChildren().addAll(imageView, imageView2);
 
         Scene scene = new Scene(stackPane);
-
+        // Hier werden Beschränkungen für Fenstergröße und der Titelname gesetzt.
         primaryStage.setTitle("Ei oder Zwei!");
         primaryStage.setMaxHeight(1440);
         primaryStage.setMinHeight(1000);
