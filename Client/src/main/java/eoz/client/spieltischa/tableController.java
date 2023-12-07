1cfa5ad4d9a9a2ba9b4e4ee8dcd791b7afb3718c
Frank Ikenna Okeke <okeke@rhrk.uni-kl.de>
2023-12-07 18:28:07 +0100

    Classes Name Refactor

--- Client/src/main/java/eoz/client/spieltischa/tableController.java
+++ Client/src/main/java/eoz/client/spieltischa/tableController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class HelloController {
+public class tableController {
     @FXML
     private Label welcomeText;
     @FXML
