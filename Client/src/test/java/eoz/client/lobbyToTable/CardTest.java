62d122b16aa6870f9a969a3ede14652a6ba3dc0f
Denis Ahmeti <d_ahmeti20@cs.uni-kl.de>
2023-12-09 16:53:20 +0100

    Changed Image to String (not final) Added tests

--- Client/src/test/java/eoz/client/lobbyToTable/CardTest.java
+++ Client/src/test/java/eoz/client/lobbyToTable/CardTest.java
@@ -1,30 +1,30 @@
 package eoz.client.lobbyToTable;
 
+import javafx.scene.image.Image;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CardTest {
-
-
-     void  getValue() {
-
-    }
-
-
-
+class CardTest{
+    Card test = new Card(1999,"test","resources/image/Ei.jpg",42);
 
     @Test
     void getID() {
+        assertEquals(test.getID(), 1999);
     }
 
     @Test
     void getType() {
+        assertEquals(test.getType(), "test");
     }
 
     @Test
     void getImage() {
+        assertEquals(test.getImage(), "resources/image/Ei.jpg");
     }
 
-
+    @Test
+    void getValue() {
+        assertEquals(test.getValue(), 42);
+    }
 }
