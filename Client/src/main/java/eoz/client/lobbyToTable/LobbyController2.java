84de88f59b88ba1566ca47a71cefa3aaa788d504
Frank Ikenna Okeke <okeke@rhrk.uni-kl.de>
2023-12-10 17:35:31 +0100

    skeletons for the "new Game" and "load game" scene

---
+++ Client/src/main/java/eoz/client/lobbyToTable/LobbyController2.java
@@ -0,0 +1,22 @@
+package eoz.client.lobbyToTable;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class LobbyController2 {
+
+
+}
