f95b14061aadeea33748522a64cb6f5e0875ae94
Jeremiah Akpor <akpor@rhrk.uni-kl.de>
2023-12-10 20:57:17 +0100

    finished lobbyStage2

--- Client/src/main/java/eoz/client/lobbyToTable/LobbyController.java
+++ Client/src/main/java/eoz/client/lobbyToTable/LobbyController.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
@@ -52,22 +54,54 @@ public class LobbyController {
     }
 
     public void switchToScene2(ActionEvent event) throws IOException {
+
+
+
+        try {
+            // Set and show the Stage
             String username = usernameField.getText();
 
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("tableView.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("lobbyStage2.fxml"));
             root = loader.load();
 
-            tableController tableController = loader.getController();
-            tableController.displayName(username);
+            LobbyController2 LobbyController2 = loader.getController();
 
-           // root = FXMLLoader.load(getClass().getResource(("tableView.fxml")));
+            LobbyController2.welcome.setText("Welcome " + username);
+            LobbyController2.username = username;
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-            tableController.setPrimaryStage(stage);
-            tableController.bindImageViewSize();
-            scene = new Scene(root);
-            root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("tablestyle.css")).toExternalForm());
-            stage.setScene(scene);
+
+            Scene scene2 = new Scene(root,800,600);
+            stage.setScene(scene2);
             stage.show();
+            stage.setTitle("Lobby");
+
+            // Assuming the ImageView has the fx:id="backgroundView" in your FXML file
+            ImageView backgroundView = (ImageView) root.lookup("#backgroundView");
+
+            // Ensure the image covers the entire StackPane area
+            backgroundView.fitWidthProperty().bind(stage.widthProperty());
+            backgroundView.fitHeightProperty().bind(stage.heightProperty());
+
+            // Remove the preserveRatio to allow the image to cover the entire area
+            backgroundView.setPreserveRatio(false);
+
+            // Set preferred window size
+            stage.setMinWidth(800); // Minimum width of the window
+            stage.setMinHeight(600); // Minimum height of the window
+
+            // Adjust the stage size after the scene is shown to ensure proper layout
+            stage.sizeToScene();
+            AnchorPane card = (AnchorPane) scene2.lookup("#card");
+            if (card != null) {
+                // Bind the card's layoutXProperty to keep it centered
+                card.layoutXProperty().bind(scene2.widthProperty().subtract(card.widthProperty()).divide(2));
+                // Bind the card's layoutYProperty to keep it at the same relative position from the top
+                card.layoutYProperty().bind(scene2.heightProperty().multiply(599.0 / 1080.0));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- Client/src/main/java/eoz/client/lobbyToTable/LobbyController2.java
+++ Client/src/main/java/eoz/client/lobbyToTable/LobbyController2.java
@@ -9,7 +9,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -18,5 +20,61 @@ import java.util.Objects;
 
 public class LobbyController2 {
 
+    public ImageView backgroundView;
+    @FXML
+    Label welcome;
+
+
+    private Stage stage;
+    private Scene scene;
+
+    private Parent root;
+
+    public String username;
+
+    @FXML
+    void exitGame(MouseEvent event) {
+        // Display a confirmation dialog before exiting the game
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit Game");
+        alert.setHeaderText("Are you sure you want to exit the game?");
+        alert.setContentText("Any unsaved progress may be lost.");
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                // User clicked OK, exit the game gracefully
+                Platform.exit();
+            }
+            // User clicked Cancel or closed the dialog, do nothing
+        });
+    }
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(("lobbyStage2.fxml"))));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("tableView.fxml"));
+        root = loader.load();
+
+        tableController tableController = loader.getController();
+        tableController.displayName(username);
+
+        // root = FXMLLoader.load(getClass().getResource(("tableView.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        tableController.setPrimaryStage(stage);
+        tableController.bindImageViewSize();
+        scene = new Scene(root);
+        root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("tablestyle.css")).toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+
+    }
 
 }
