fdb4160ba135d75547edb4af57f36bf96305ce2d
Denis Ahmeti <d_ahmeti20@cs.uni-kl.de>
2023-12-12 17:42:48 +0100

    Added tests and removed Deck method

---
+++ Client/src/test/java/eoz/client/lobbyToTable/DeckTest.java
@@ -0,0 +1,12 @@
+package eoz.client.lobbyToTable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @Test
+    void pop() {
+    }
+}
---
+++ Client/src/test/java/eoz/client/lobbyToTable/SpiellogikTest.java
@@ -0,0 +1,48 @@
+package eoz.client.lobbyToTable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpiellogikTest {
+
+    @Test
+    void getKartendeck() {
+    }
+
+    @Test
+    void spielzugManager() {
+    }
+
+    @Test
+    void spielzugMoeglichkeiten() {
+    }
+
+    @Test
+    void spielzug() {
+    }
+
+    @Test
+    void zieheKarte() {
+    }
+
+    @Test
+    void multiselect() {
+    }
+
+    @Test
+    void spieleranfrage() {
+    }
+
+    @Test
+    void hahnklauen() {
+    }
+
+    @Test
+    void fuchsklau() {
+    }
+
+    @Test
+    void eilegen() {
+    }
+}
