0303cce482a76a307d1e384a5436bac00234e3c3
Franklin Ikenna Okeke <f_okeke20@cs.uni-kl.de>
2023-12-15 15:01:51 +0000

    Revert "Merge branch 'gameLogic' into 'main'"
    This reverts merge request !16

--- Server/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module eoz.server.server {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens eoz.server.server to javafx.fxml;
-    exports eoz.server.server;
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-
-//DOOT DOOT
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
