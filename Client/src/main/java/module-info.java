b037edfef366a55bf37c1af37dbdc715ab76448a
Frank Ikenna Okeke <okeke@rhrk.uni-kl.de>
2023-12-22 19:52:16 +0100

    Lobby repaired. (D, T, L, A)

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,14 +1,13 @@
-module eoz.client.client {
+module eoz.client.lobbyToTable {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
     requires java.rmi;
 
 
-    opens eoz.client.client to javafx.fxml;
+
     opens eoz.client.lobbyToTable to javafx.fxml;
     opens eoz.client.spielbrett to javafx.fxml;
-    exports eoz.client.client;
     exports eoz.client.lobbyToTable;
     exports eoz.client.spielbrett;
 }
