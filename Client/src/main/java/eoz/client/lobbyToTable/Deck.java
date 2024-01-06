8ccc3cee9282e8b8a4a0eb0ad94aedc32e7ddc90
Frank Ikenna Okeke <okeke@rhrk.uni-kl.de>
2024-01-06 16:36:59 +0100

    images refactor

--- Client/src/main/java/eoz/client/lobbyToTable/Deck.java
+++ Client/src/main/java/eoz/client/lobbyToTable/Deck.java
@@ -1,9 +1,6 @@
 package eoz.client.lobbyToTable;
 
-import javafx.scene.image.Image;
-
 import javax.swing.*;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Stack;
 
@@ -18,14 +15,14 @@ public class Deck {
 
         for (int i = 1; i < 11; i++) {
             deck.push(new Card(index++, "Koerner", new ImageIcon(getClass().getResource("/cards/zwei.png").toString()), 2));
-            deck.push(new Card(index++, "Koerner", new ImageIcon(getClass().getResource("/cards/dreik.png").toString()), 3));
+            deck.push(new Card(index++, "Koerner", new ImageIcon(getClass().getResource("/cards/drei.png").toString()), 3));
             deck.push(new Card(index++, "Koerner", new ImageIcon(getClass().getResource("/cards/vier.png").toString()), 4));
             deck.push(new Card(index++, "Biokörner", new ImageIcon(getClass().getResource("/cards/zweib.png").toString()), 2));
-            deck.push(new Card(index++, "Biokörner", new ImageIcon(getClass().getResource("/cards/drei.png").toString()), 3));
+            deck.push(new Card(index++, "Biokörner", new ImageIcon(getClass().getResource("/cards/dreib.png").toString()), 3));
         }
 
         for (int j = 1; j < 9; j++) {
-            deck.push(new Card(index++, "Biokörner", new ImageIcon(getClass().getResource("/cards/drei.png").toString()), 1));
+            deck.push(new Card(index++, "Biokörner", new ImageIcon(getClass().getResource("/cards/dreib.png").toString()), 1));
         }
 
         for (int k = 1; k < 13; k++) {
