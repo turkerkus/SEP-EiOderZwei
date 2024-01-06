fabc6ee224b16be2f2777806b8cee1dcd42b7f4b
Lars Schimmelpfennig <lschimm@rhrk.uni-kl.de>
2024-01-06 18:17:52 +0100

    Ablagestapel

---
+++ Client/src/main/java/eoz/client/lobbyToTable/Ablagestapel.java
@@ -0,0 +1,23 @@
+package eoz.client.lobbyToTable;
+
+import javax.swing.*;
+import java.util.Collections;
+import java.util.Stack;
+
+public class Ablagestapel {
+
+    private Stack<Card> ablagestapel;
+
+
+    public Ablagestapel() {
+        ablagestapel = new Stack<>();
+
+    }
+    public void pushCardAblagestapel(Card card){
+        ablagestapel.push(card);
+    }
+    public Card readFirstElement() {
+        return  ablagestapel.peek();
+    }
+
+}
